d6da93f93aa8c0dfef7b7f0ce616adfcaf16da2a
woochul2 <leewoo0686@gmail.com>
2021-04-27 22:53:28 +0900

    refactor: typescript로 변환 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Home from './pages/Home';
 
 export default function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import 'normalize.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/reset.css';
