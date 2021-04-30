9150ce26d7e4181202e4861272bec44174967ab0
woochul2 <leewoo0686@gmail.com>
2021-04-30 15:13:44 +0900

    refactor: Home styles 정리

--- src/pages/Home/styles/Home.ts
+++ src/pages/Home/styles/Home.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/macro';
 import * as BREAKPOINTS from '../../../constants/breakpoints';
 
-const sidePadding = '3.25rem';
-const smallSidePadding = '2rem';
+const sidePadding = {
+  default: '3.25rem',
+  SM: '2rem;',
+};
 
 export const Container = styled.div`
   min-width: 18.75rem;
@@ -11,36 +13,34 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.header`
-  // 모달을 제외하고 가장 위에 위치하도록 z-index 설정
+  // 모달을 제외한 나머지 중에서 가장 위에 위치하도록 z-index 설정
   z-index: 3;
   position: sticky;
   top: 0;
   left: 0;
-  right: 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0.875rem ${sidePadding};
+  padding: 0.875rem ${sidePadding.default};
   background-image: linear-gradient(hsla(0, 0%, 0%, 0.7) 10%, hsla(0, 0%, 0%, 0));
   transition: background-color 0.4s;
 
   &.scroll-down {
-    background: var(--gray-900);
+    background-color: var(--gray-900);
   }
 
   @media (max-width: ${BREAKPOINTS.SM}) {
-    padding: 0.875rem ${smallSidePadding};
+    padding: 0.875rem ${sidePadding.SM};
   }
 `;
 
 export const LogoLink = styled.a`
   user-select: none;
-  margin: 0;
   text-decoration: none;
-  font-size: 3rem;
   font-weight: bold;
-  color: var(--red);
   font-size: 2rem;
+  color: var(--red);
+
+  @media (max-width: ${BREAKPOINTS.MD}) {
+    font-size: 1.5rem;
+  }
 
   @media (max-width: ${BREAKPOINTS.SM}) {
     font-size: 1.25rem;
@@ -48,10 +48,10 @@ export const LogoLink = styled.a`
 `;
 
 export const Main = styled.main`
-  padding: 0 ${sidePadding} 5rem;
+  padding: 0 ${sidePadding.default} 5rem;
 
   @media (max-width: ${BREAKPOINTS.SM}) {
-    padding: 0 ${smallSidePadding};
+    padding: 0 ${sidePadding.SM};
   }
 `;
 
@@ -60,8 +60,8 @@ export const Notification = styled.div`
   font-weight: bold;
   font-size: 1.25rem;
   margin-bottom: 4rem;
-  color: var(--orange);
   background-color: var(--gray-800);
+  color: var(--orange);
 
   @media (max-width: ${BREAKPOINTS.LG}) {
     font-size: 1rem;
@@ -75,6 +75,6 @@ export const Notification = styled.div`
 
 export const TMDbLogo = styled.a`
   display: inline-block;
-  width: 10em;
-  margin: 0 0.2em 0 0.3em;
+  width: 12.5rem;
+  margin: 0 0.25rem 0 0.375rem;
 `;
