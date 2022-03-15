1f6b98bf228cf68f80e5395a6e2878cd2b608af5
Woochul Lee <leewoo0686@gmail.com>
2022-03-15 15:18:39 +0900

    fix: 제목 글자 원치 않는 줄바꿈 방지
    제목 글자 수가 몇 개 안되는데도 줄바꿈이 되는 경우가 있어서 그냥 width를 100%로 설정했다. 모바일에서만 그런데, 이유는 모르겠다. (position을 absolute로 했을 때만 그렇다.)

--- src/components/ContentTitle.js
+++ src/components/ContentTitle.js
@@ -5,24 +5,22 @@ import styled from 'styled-components';
  * @param {string} props.name
  */
 function ContentTitle({ name }) {
-  return <Title className={`${name.length < 7 && 'short'}`}>{name}</Title>;
+  return <Title>{name}</Title>;
 }
 
 const Title = styled.h3`
   user-select: none;
   cursor: pointer;
   position: absolute;
-  bottom: 0.3em;
-  right: 0.4em;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  padding: 0.3em 0.4em;
   text-align: right;
   font-family: 'Nanum Brush Script', cursive;
   font-size: 1em;
   mix-blend-mode: luminosity;
   background: radial-gradient(hsla(0, 0%, 0%, 0.5), hsla(0, 0%, 0%, 0) 100%);
-
-  &.short {
-    background: radial-gradient(hsla(0, 0%, 0%, 0.5), hsla(0, 0%, 0%, 0) 70%);
-  }
 `;
 
 export default ContentTitle;
